80ae4fa6a40058e05f655fb8bf72437cc8db3b0a
HangaAD <165654997+HangaAD@users.noreply.github.com>
2025-08-19 09:25:15 -0700

    Update problema7_criba.py

--- problema7_criba.py
+++ problema7_criba.py
@@ -3,8 +3,17 @@
 from typing import List
 
 def sieve(n: int) -> List[int]:
-    # TODO
-    return []
+    prim = [True] * (n + 1)
+    prim[0] = False
+    prim[1] = False
+    p = 2
+    while p * p <= n:
+        if prim[p] == True:
+            for i in range(p * p, n + 1, p):
+                prim[i] = False
+        p += 1
+    return [i for i, is_prim in enumerate(prim) if is_prim]
 
 if __name__ == "__main__":
-    print(sieve(20))  # [2, 3, 5, 7, 11, 13, 17, 19]
+    print(sieve(20))
+#pentru sieve(30) [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
