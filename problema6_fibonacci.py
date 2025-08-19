136c1b9f24786ac99261c0d56ee2d7c377b33a4c
HangaAD <165654997+HangaAD@users.noreply.github.com>
2025-08-19 09:05:34 -0700

    Update problema6_fibonacci.py

--- problema6_fibonacci.py
+++ problema6_fibonacci.py
@@ -1,8 +1,12 @@
 # Problema 6 – Fibonacci recursiv (10p)
 
 def fib(n: int) -> int:
-    # TODO
-    return 0
+    if n==0 or n==1:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
 
 if __name__ == "__main__":
     print(fib(6))  # 8
+#fib(10) returneaza 55
+
