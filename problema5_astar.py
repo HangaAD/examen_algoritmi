47e631e9c8652aa45e635177c42ca10bad3902d9
HangaAD <165654997+HangaAD@users.noreply.github.com>
2025-08-19 09:57:14 -0700

    Update problema5_astar.py

--- problema5_astar.py
+++ problema5_astar.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
         [0,0,0]
     ]
     print(astar(grid, (0,0), (2,2)))
-#A* este ceva intre greedy si Dijktra in sensul in care din nodul curent o sa caute toate nodurile posibile catre final dar se lasa ghidat de o euristica aproximativa 
+#A* este ceva intre greedy si Dijktra in sensul in care din nodul curent o sa caute toate nodurile posibile( se va uita la obstacole) catre final dar se lasa ghidat de o euristica aproximativa 
 # e un fel de cel mai bun drum pana aici, tinand cont ca urmatoarele probabil vor fi ok adica f= cost total daca mergi prin n, g cat a costat sa ajungi acolo si h cat crezi ca te va mai costa
 # iar euristica aici e distanta dintre punctul meu si punctul final 
