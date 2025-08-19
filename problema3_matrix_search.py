ea9f1fa109c5540f4dc7fd682da3e0a30c53eeb1
HangaAD <165654997+HangaAD@users.noreply.github.com>
2025-08-19 08:56:09 -0700

    Update problema3_matrix_search.py

--- problema3_matrix_search.py
+++ problema3_matrix_search.py
@@ -1,10 +1,21 @@
 # Problema 3 – Căutare într-o matrice sortată (15p)
-
 from typing import List
 
 def search_sorted_matrix(mat: List[List[int]], x: int) -> bool:
-    # TODO
-    return False
+    found=False
+    row=len(mat)
+    col=len(mat[0])
+    i=0
+    j=col-1
+    while i < row and j>=0:
+        if mat[i][j]==x:
+            found=True
+            break
+        elif mat[i][j]>x:
+            j=j-1
+        else:
+            i=i+1
+    return found
 
 if __name__ == "__main__":
     mat = [
@@ -12,4 +23,6 @@ if __name__ == "__main__":
         [2, 5, 9],
         [3, 6, 10]
     ]
-    print(search_sorted_matrix(mat, 5))  # True
+    print(search_sorted_matrix(mat, 5))
+#Codul incepe de la coltul drepta sus si daca vede ca elementul curent e mai mare inseamca ca ma mut pe o coloana cu elemente mai mic deci la stanga si daca vede ca elementul e mai mic 
+#se duce pe urmatoarea linie unde sunt elemnte mai mari decat cel curent 
